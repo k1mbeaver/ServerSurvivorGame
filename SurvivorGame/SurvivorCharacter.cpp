e3f3e47eb5dcc9895787b22ee1e43e975e39fd34
ksw4811 <ksw4811@naver.com>
2022-12-25 16:22:01 +0900

    캐릭터와 플레이어 컨트롤러

---
+++ SurvivorGame/SurvivorCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SurvivorCharacter.h"
+
+// Sets default values
+ASurvivorCharacter::ASurvivorCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASurvivorCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASurvivorCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASurvivorCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
