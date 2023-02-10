7cfca0f90b6eabede2cee3a500dc2e1a4eef54cc
ksw4811 <ksw4811@naver.com>
2023-02-10 22:57:54 +0900

    UI 사용을 위한 기초 작업

---
+++ SurvivorGame/PlayerWeaponUI.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerWeaponUI.h"
+
+PlayerWeaponUI::PlayerWeaponUI()
+{
+}
+
+PlayerWeaponUI::~PlayerWeaponUI()
+{
+}
